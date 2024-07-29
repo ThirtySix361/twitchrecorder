6dfe90868ea225cfff174c3cfa450b97a608a87e
thirtysix <git@36ip.de>
2024-07-29 20:54:37 +0200

    v1.0.2 updated index.php
    small code refactor
    added navigation buttons on side
    updated README.md

--- README.md
+++ README.md
@@ -1,25 +1,29 @@
-# twitchrecorder
+# 🎥 twitchrecorder
 
-[![mail](https://img.shields.io/badge/Contact-dev%4036ip.de-blue?logo=maildotru)](mailto:dev@36ip.de)
-![discord](https://img.shields.io/badge/Discord-.thirtysix-5865F2?style=flat&logo=discord)
-
-[![version](https://img.shields.io/badge/Version-1.0.1-deepgreen)](https://github.com/ThirtySix361/twitchrecorder?tab=readme-ov-file)
-[![commit](https://img.shields.io/github/last-commit/ThirtySix361/twitchrecorder)](https://github.com/ThirtySix361/twitchrecorder?tab=readme-ov-file)
-[![stars](https://img.shields.io/github/stars/thirtysix361/twitchrecorder.svg?logo=github&style=flat)](https://github.com/ThirtySix361/twitchrecorder?tab=readme-ov-file)
+[![version](https://img.shields.io/badge/version-1.0.2-deepgreen)](https://github.com/ThirtySix361/twitchrecorder)
+[![commit](https://img.shields.io/github/last-commit/ThirtySix361/twitchrecorder?logo=github&label=github+last+commit)](https://github.com/ThirtySix361/twitchrecorder)
+[![stars](https://img.shields.io/github/stars/thirtysix361/twitchrecorder.svg?logo=github&style=flat&label=github+stars)](https://github.com/ThirtySix361/twitchrecorder)
 [![pulls](https://img.shields.io/docker/pulls/thirtysix361/twitchrecorder.svg?logo=docker)](https://hub.docker.com/r/thirtysix361/twitchrecorder)
 [![stars](https://img.shields.io/docker/stars/thirtysix361/twitchrecorder.svg?logo=docker)](https://hub.docker.com/r/thirtysix361/twitchrecorder)
 
-<hr>
+[![mail](https://img.shields.io/badge/contact-dev%4036ip.de-blue?logo=maildotru)](mailto:dev@36ip.de)
+![discord](https://img.shields.io/badge/discord-.thirtysix-5865F2?style=flat&logo=discord)
+
+---
 
 this container lets you download any twitch stream by giving a simple twitch channel name as parameter.
 
-![preview](preview.png)
+[![preview](https://raw.githubusercontent.com/ThirtySix361/twitchrecorder/master/preview.png)](https://raw.githubusercontent.com/ThirtySix361/twitchrecorder/master/preview.png)
 
-## dependencies
+---
+
+## 🔗 dependencies
 
 + docker
 
-## quick start
+---
+
+## 🚀 quick start
 
 step 1.
 
@@ -51,9 +55,11 @@ bash run.sh pewdiepie
 bash run.sh montanablack88
 ```
 
->//<br><samp>💡 optionally point your webserver with php interpreter to the `mounts/` directory </samp><br>//
+>//<br>//<samp> 💡 optionally point your webserver with php interpreter to the `mounts/` directory </samp><br>//
 
-## general informations
+---
+
+## 🧠 general informations
 
 this container will scan every 5 minutes if the given streamer has started streaming. \
 if so, it will start recording the stream.
@@ -66,7 +72,11 @@ on common stream ending, the container will perform a fix on the final `.mp4` fi
 
 all header information are stored at the begin of the file which is uncommon. but only by this way it is possible to have a working file after crash or forceful shutdown of the container and its inner capturing software.
 
-## troubleshooting
+>//<br>//<samp> 💡 the `.mp4` files are stored in the `mounts/archive/<channelname>/` directory </samp><br>//
+
+---
+
+## 🧐 troubleshooting
 
 in case a container was forcefully killed and you want to fix the files, use `fix` as second parameter:
 
@@ -86,3 +96,31 @@ startupfix="fix"
 ```
 
 this will fix all files for a streamer on startup, which can take a while.
+
+---
+
+## 📝 todo list
+
+- [x] container
+    - [x] runs as user instead of root
+    - [x] allowing multiple instances at same time for unlimited parallel recordings
+    - [x] proper logging
+- [ ] recordings
+    - [x] autodetect if streamer started streaming and start recording
+    - [x] make file playable even if the streamer is still streaming and file is still being written
+    - [x] prevent file from beeing corrupted after container shutdown while file is still being written
+    - [x] fix header information of final .mp4 file on stream end
+    - [x] optional re-fix files on container startup
+        - [ ] fix only files which are not already fine
+    - [ ] capture chat into textfile
+    - [ ] improve live playback
+- [ ] webpage
+    - [x] list every video file from archive (order by filename)
+    - [x] display filename and filesize
+    - [x] delete video button
+    - [x] video navigation buttons
+    - [x] save last time position to localstorage
+    - [x] load last time position on open
+    - [x] remove last time position on delete
+    - [ ] display chat next to the video
+        - [ ] sync chat with video
--- mounts/index.php
+++ mounts/index.php
@@ -14,29 +14,26 @@
 
     function getFiles($dir) {
         $files = [];
-        $items = scandir($dir);
+        $items = new DirectoryIterator($dir);
         foreach ($items as $item) {
-            if ($item != '.' && $item != '..') {
-                $path = $dir . DIRECTORY_SEPARATOR . $item;
-                if (is_file($path) && pathinfo($path, PATHINFO_EXTENSION) === 'mp4') {
-                    $files[] = [
-                        'path' => $path,
-                        'date' => filemtime($path),
-                        'name' => $item,
-                        'size' => filesize($path)
-                    ];
-                } elseif (is_dir($path)) {
-                    $files = array_merge($files, getFiles($path));
-                }
+            if ($item->isDot()) continue;
+            $path = $item->getPathname();
+            if ($item->isFile() && $item->getExtension() === 'mp4') {
+                $files[] = [
+                    'path' => $path,
+                    'date' => $item->getMTime(),
+                    'name' => $item->getFilename(),
+                    'size' => $item->getSize()
+                ];
+            } elseif ($item->isDir()) {
+                $files = array_merge($files, getFiles($path));
             }
         }
         return $files;
     }
 
     $videoFiles = getFiles(__DIR__);
-    usort($videoFiles, function($a, $b) {
-        return strcmp($b['name'], $a['name']);
-    });
+    usort($videoFiles, fn($a, $b) => strcmp($b['name'], $a['name']));
 ?>
 <!DOCTYPE html>
 <html lang="de">
@@ -79,22 +76,78 @@
         .options button {
             padding: 5px 10px;
             cursor: pointer;
-            background-color: #999;
+            background-color: #555;
+            color: #999;
             border: 0;
             border-radius: 3px;
         }
+        .navigation {
+            position: fixed;
+            right: 50px;
+            top: 50%;
+            transform: translateY(-50%);
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+        }
+        .navigation button {
+            padding: 10px 20px;
+            margin: 5px 0;
+            cursor: pointer;
+            background-color: #555;
+            color: #999;
+            border: 0;
+            border-radius: 3px;
+        }
+        .navigation button:hover, .options button:hover {
+            background-color: #666;
+        }
+        .counter {
+            margin: 10px 0;
+        }
     </style>
     <script>
-        function openInNewTab(url) {
-            window.open(url, '_blank');
+        let currentIndex = 0;
+        let videoTiles = [];
+
+        document.addEventListener("DOMContentLoaded", () => {
+            videoTiles = document.querySelectorAll('.video-tile');
+            updateCounter();
+
+            document.querySelector('#btnUp').addEventListener('click', () => scrollToVideo(currentIndex - 1));
+            document.querySelector('#btnDown').addEventListener('click', () => scrollToVideo(currentIndex + 1));
+
+            window.addEventListener('scroll', updateCounter);
+
+            document.querySelectorAll('video').forEach(video => {
+                loadVideoTime(video);
+
+                const saveTime = () => saveVideoTime(video);
+
+                video.addEventListener('play', () => video.addEventListener('timeupdate', saveTime));
+                video.addEventListener('pause', () => video.removeEventListener('timeupdate', saveTime));
+                video.addEventListener('ended', () => video.removeEventListener('timeupdate', saveTime));
+            });
+        });
+
+        function scrollToVideo(index) {
+            currentIndex = Math.max(0, Math.min(index, videoTiles.length - 1));
+            videoTiles[currentIndex].scrollIntoView({ behavior: 'smooth', block: 'center' });
+            updateCounter();
+        }
+
+        function updateCounter() {
+            videoTiles.forEach((tile, index) => {
+                const rect = tile.getBoundingClientRect();
+                if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+                    currentIndex = index;
+                }
+            });
+            document.querySelector('#counter').textContent = `${currentIndex + 1} / ${videoTiles.length}`;
         }
 
         function saveVideoTime(video) {
             localStorage.setItem(video.src, video.currentTime);
-        }
-
-        function removeVideoTime(video) {
-            localStorage.removeItem(video.src);
         }
 
         function loadVideoTime(video) {
@@ -104,32 +157,11 @@
             }
         }
 
-        document.addEventListener("DOMContentLoaded", () => {
-            document.querySelectorAll('video').forEach(video => {
-                loadVideoTime(video);
-
-                const saveTime = () => saveVideoTime(video);
-
-                video.addEventListener('play', function() {
-                    video.addEventListener('timeupdate', saveTime);
-                });
-
-                video.addEventListener('pause', function() {
-                    video.removeEventListener('timeupdate', saveTime);
-                });
-
-                video.addEventListener('ended', function() {
-                    video.removeEventListener('timeupdate', saveTime);
-                });
-
-            });
-        });
-
         function deleteFile(filePath, videourl) {
-            if (confirm('Are you sure, you want to delete this file?')) {
-                video = document.querySelector(`video[src="${videourl}"]`)
-                removeVideoTime(video)
-                fetch(window.location.href + '?delete=' + encodeURIComponent(filePath))
+            if (confirm('Are you sure you want to delete this file?')) {
+                const video = document.querySelector(`video[src="${videourl}"]`);
+                localStorage.removeItem(video.src);
+                fetch(`?delete=${encodeURIComponent(filePath)}`)
                     .then(response => response.text())
                     .then(data => {
                         console.log(data);
@@ -140,25 +172,28 @@
     </script>
 </head>
 <body>
+    <div id="header" style="padding: 25px; position: fixed; top: 0; left:0; right: 0; text-align: center; background-color: #444">
+        <a href="/archive" style="text-decoration: underline; color: #aaa;">archive</a>
+    </div>
 
-    <div id="header" style="padding: 25px; position: fixed; top: 0; left:0; right: 0; text-align: center; background-color: #444"><a href="/archive" style="text-decoration: underline; color: #aaa;">archive</a></div>
+    <div class="navigation">
+        <button id="btnUp">▲</button>
+        <div id="counter" class="counter">1 / <?php echo count($videoFiles); ?></div>
+        <button id="btnDown">▼</button>
+    </div>
 
-    <?php
-        foreach ($videoFiles as $video) {
-            $videoUrl = str_replace(__DIR__, '', $video['path']);
-            $fileName = basename($video['path']);
-            $fileSizeGB = number_format($video['size'] / (1024 * 1024 * 1024), 2);
-            echo '<div class="video-tile">';
-            echo '<div class="video-name">' . htmlspecialchars($fileName) . ' <br> ' . ' (' . $fileSizeGB . ' GB)</div>';
-            echo '<video class="video-thumb" src="' . $videoUrl . '" controls></video>';
-            echo '<div class="options">';
-            echo '<button onclick="deleteFile(\'' . $video['path'] . '\', \'' . $videoUrl . '\')">Delete</button>';
-            echo '</div>';
-            echo '</div>';
-        }
-    ?>
+    <?php foreach ($videoFiles as $video): ?>
+        <div class="video-tile">
+            <div class="video-name"><?= htmlspecialchars($video['name']) ?> <br> (<?= number_format($video['size'] / (1024 * 1024 * 1024), 2) ?> GB)</div>
+            <video class="video-thumb" src="<?= str_replace(__DIR__, '', $video['path']) ?>" controls></video>
+            <div class="options">
+                <button onclick="deleteFile('<?= $video['path'] ?>', '<?= str_replace(__DIR__, '', $video['path']) ?>')">Delete</button>
+            </div>
+        </div>
+    <?php endforeach; ?>
 
-    <div id="footer" style="padding: 25px; position: fixed; bottom: 0; left:0; right: 0; text-align: center; background-color: #444">by 36</div>
-
+    <div id="footer" style="padding: 25px; position: fixed; bottom: 0; left:0; right: 0; text-align: center; background-color: #444">
+        by <a href="https://github.com/ThirtySix361/twitchrecorder" style="text-decoration: underline; color: #aaa;">thirtysix</a>
+    </div>
 </body>
 </html>
